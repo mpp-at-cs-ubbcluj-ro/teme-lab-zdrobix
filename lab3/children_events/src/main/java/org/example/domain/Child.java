08fef24529f656f2b6e0f088fcade923478bbd0f
zdrobix <zdroba88@gmail.com>
2025-03-08 12:01:58 +0200

    Added domain classes and repo interface for lab3.

---
+++ lab3/children_events/src/main/java/org/example/domain/Child.java
@@ -0,0 +1,19 @@
+package org.example.domain;
+
+public class Child extends Entity<Integer>{
+    private String Name;
+    private String CNP;
+
+    public Child (String name, String cnp) {
+        Name = name;
+        CNP = cnp;
+    }
+
+    public String GetName() {
+        return this.Name;
+    }
+
+    public String GetCNP() {
+        return this.CNP;
+    }
+}
